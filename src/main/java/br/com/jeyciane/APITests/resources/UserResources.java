37861330da5e7f26ee2d4d366bebaeae38402e4c
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-05 19:10:03 -0300

    Adicionar método findById

---
+++ src/main/java/br/com/jeyciane/APITests/resources/UserResources.java
@@ -0,0 +1,19 @@
+package br.com.jeyciane.APITests.resources;
+
+import br.com.jeyciane.APITests.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserResources {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(new User(1, "Jey","jey@zup.com","123"));
+    }
+
+}
