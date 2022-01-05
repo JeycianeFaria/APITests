cdc6ed4d21f3e44e46838aba737f2b495d917a56
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-05 19:27:23 -0300

    Injetar user service na user resources

--- src/main/java/br/com/jeyciane/APITests/resources/UserResources.java
+++ src/main/java/br/com/jeyciane/APITests/resources/UserResources.java
@@ -1,6 +1,8 @@
 package br.com.jeyciane.APITests.resources;
 
 import br.com.jeyciane.APITests.domain.User;
+import br.com.jeyciane.APITests.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserResources {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Jey","jey@zup.com","123"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
+
 
 }
