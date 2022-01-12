3eb00c29f6e7a852c88093fd599050b7912e53d4
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-12 15:34:45 -0300

    Update

--- src/main/java/br/com/jeyciane/APITests/resources/UserResources.java
+++ src/main/java/br/com/jeyciane/APITests/resources/UserResources.java
@@ -49,4 +49,10 @@ public class UserResources {
         return ResponseEntity.created(uri).build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj){
+        obj.setId(id);
+        return ResponseEntity.ok().body(modelMapper.map(userService.update(obj),UserDTO.class));
+    }
+
 }
