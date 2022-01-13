90ebcdcf22c5b2075073aece973241d8147b5ae0
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-13 08:13:43 -0300

    Teste service user método find all

--- src/test/java/br/com/jeyciane/APITests/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/jeyciane/APITests/services/impl/UserServiceImplTest.java
@@ -14,6 +14,7 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +29,7 @@ class UserServiceImplTest {
     public static final String EMAIL = "jey@zup.com";
     public static final String PASSWORD = "123";
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final int INDEX = 0;
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -73,7 +75,19 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(userRepository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = userService.findAll();
+
+        assertNotNull(response);
+        assertEquals(1,response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NOME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
     }
 
     @Test
