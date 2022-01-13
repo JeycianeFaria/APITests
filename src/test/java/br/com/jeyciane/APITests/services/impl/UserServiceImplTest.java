a1a290849890b3ebd534056b2a0353e2db92e34e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-13 08:29:26 -0300

    Teste service user método create sucesso

--- src/test/java/br/com/jeyciane/APITests/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/jeyciane/APITests/services/impl/UserServiceImplTest.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -91,7 +92,18 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSucess() {
+        when(userRepository.save(any())).thenReturn(user);
+
+        User response = userService.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class,response.getClass());
+        assertEquals(ID,response.getId());
+        assertEquals(NOME,response.getName());
+        assertEquals(EMAIL,response.getEmail());
+        assertEquals(PASSWORD,response.getPassword());
+
     }
 
     @Test
